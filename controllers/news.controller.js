69e0a05c141c63c62e81f1a8d8f7bdddcc7c7170
Anthony Lizano <jalu_27@hotmail.com>
2023-04-30 18:09:53 -0600

    here both function were created
    filterNewsByUserId and getNews by search

---
+++ controllers/news.controller.js
@@ -0,0 +1,47 @@
+import { newsModel } from "../models/news.model.js";
+
+
+export const filterNewsByUserId = async function (userId) {
+  try {
+
+
+    const news = await newsModel.find().populate('user').populate('category').populate('tags');
+    const filteredNews = news.filter(item => item.user._id === userId);
+
+
+
+    if (filteredNews) {
+
+      return filteredNews;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log('Error retrieving news: ', error);
+    return null;
+  }
+}
+
+export const getNews = async function (userId, search, categoryId, tags) {
+  console.log("userId");
+  console.log(userId);
+  try {
+   
+    const news = await newsModel.find().populate('user').populate('category').populate('tags');
+    const filteredNews = news.filter(item => {
+      const titleMatch = item.title.toLowerCase().includes(search.toLowerCase());
+      const descMatch = item.description.toLowerCase().includes(search.toLowerCase());
+      return item.user._id === userId && (titleMatch || descMatch);
+    });
+    if (filteredNews) {
+      console.log("filteredNews");
+      console.log(filteredNews);
+      return filteredNews;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log('Error retrieving news: ', error);
+    return null;
+  }
+};
